1dbeb7cbd930f41fbe50385f00d083c7dfb7584d
jardison17 <jallima1997@gmail.com>
2025-10-15 18:26:03 -0400

    fim

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,26 +11,45 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 600,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            child: ListTile(
-              title: Text(tr.title),
-              subtitle: Text(DateFormat('dd/MM/yyyy').format(tr.date)),
-              trailing: Text(
-                'R\$ ${tr.value.toStringAsFixed(2)}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 70, 70, 70),
-                ),
+      child:
+          transactions.isEmpty
+              ? Column(
+                children: [
+                  Text(
+                    'Nada Por Aqui',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 56, 55, 54),
+                      fontFamily: 'Lufga',
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    height: 200,
+                    child: Image.asset('imagenwaiting.png', fit: BoxFit.cover),
+                  ),
+                ],
+              )
+              : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (ctx, index) {
+                  final tr = transactions[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(tr.title),
+                      subtitle: Text(DateFormat('dd/MM/yyyy').format(tr.date)),
+                      trailing: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 70, 70, 70),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
